fa84647b9472352dc71c975416c5b212e3b10d0c
mfrancis95 <mikefrancis95@gmail.com>
2018-03-01 20:01:01 -0500

    Improve usage handling

--- findme-test.sh
+++ findme-test.sh
@@ -1,12 +1,14 @@
 show_usage() {
     echo 'Usage: findme-test url latitude longitude'
+    exit $1
 }
 
-if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
-    show_usage
+if [ $# -eq 0 ]; then
+    show_usage 1
+elif [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+    show_usage 0
 elif [ $# -lt 3 ]; then
-    show_usage
-    exit 1
+    show_usage 1
 else
     read -p 'Enter username: ' username
     read -p 'Enter password: ' -s password && echo
