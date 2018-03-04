a5514ae0c91630d6c99f2874fc0457892ce96fef
mfrancis95 <mikefrancis95@gmail.com>
2018-03-04 13:25:54 -0500

    Add username argument

--- findme-test.sh
+++ findme-test.sh
@@ -1,5 +1,10 @@
+send_location() {
+    read -p 'Enter password: ' -s password && echo
+    curl -d "{\"lat\": $2, \"lng\": $3}" -H "Content-Type: application/json" -s -u $1:$password -X POST $4
+}
+
 show_usage() {
-    echo 'Usage: findme-test url latitude longitude'
+    echo 'Usage: findme-test [-u|--username] latitude longitude url'
     exit $1
 }
 
@@ -7,10 +12,17 @@ if [ $# -eq 0 ]; then
     show_usage 1
 elif [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
     show_usage 0
-elif [ $# -lt 3 ]; then
-    show_usage 1
+elif [ "$1" == "-u" ] || [ "$1" == "--username" ]; then
+    if [ $# -lt 5 ]; then
+        show_usage 1
+    else
+        send_location ${@:2}
+    fi
 else
-    read -p 'Enter username: ' username
-    read -p 'Enter password: ' -s password && echo
-    curl -d "{\"lat\": $2, \"lng\": $3}" -H "Content-Type: application/json" -s -u $username:$password -X POST $1
+    if [ $# -lt 3 ]; then
+        show_usage 1
+    else
+        read -p 'Enter username: ' username
+        send_location $username $@
+    fi
 fi
